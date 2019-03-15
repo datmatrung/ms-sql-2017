f1aceead64ea74f4996093912f83b26ef55238c5
datmatrung <datmatrung@gmail.com>
2019-03-15 11:46:07 +0700

    LienKetBang

---
+++ SQLQuery02.sql
@@ -0,0 +1,72 @@
+CREATE DATABASE QL_SanPham
+GO
+USE QL_SanPham
+GO
+CREATE TABLE LoaiTaiKhoan(
+	MaLoaiTaiKhoan	INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
+	TenLoaiTaiKhoan	NVARCHAR(30) NOT NULL
+);
+GO
+CREATE TABLE LoaiSanPham(
+	MaLoaiSanPham	INT PRIMARY KEY NOT NULL IDENTITY(1, 1), 
+	TenLoaiSanPham	NVARCHAR(64) NOT NULL,
+	BiXoa			BIT NOT NULL
+);
+GO
+CREATE TABLE HangSanXuat(
+	MaHangSanXuat	INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
+	TenHangSanXuat	NVARCHAR(64) NOT NULL,
+	BiXoa			BIT NOT NULL
+);
+GO
+CREATE TABLE TinhTrang(
+	MaTinhTrang		INT PRIMARY KEY NOT NULL IDENTITY(1, 1), 
+	TenTinhTrang	NVARCHAR(45) NOT NULL
+);
+GO
+CREATE TABLE TaiKhoan(
+	MaTaiKhoan		INT PRIMARY KEY NOT NULL IDENTITY(1, 1), 
+	TenDangNhap		VARCHAR(30) NOT NULL, 
+	MatKhau			VARCHAR(30) NOT NULL, 
+	TenHienThi		NVARCHAR(64) NOT NULL, 
+	DiaChi			NVARCHAR(256),
+	SoDienThoai		VARCHAR(11) NOT NULL,
+	Email			VARCHAR(64) NOT NULL, 
+	BiXoa			BIT NOT NULL, 
+	MaLoaiTaiKhoan	INT NOT NULL,
+	CONSTRAINT fk_MaLoaiTaiKhoan_LoaiTaiKhoan FOREIGN KEY (MaLoaiTaiKhoan) REFERENCES LoaiTaiKhoan(MaLoaiTaiKhoan)
+);
+GO
+CREATE TABLE SanPham(
+	MaSanPham		INT PRIMARY KEY NOT NULL IDENTITY(1, 1), 
+	TenSanPham		NVARCHAR(256) NOT NULL, 
+	HinhURL			VARCHAR(256) NOT NULL, 
+	GiaSanPham		MONEY NOT NULL, 
+	SoLuongTon		INT NOT NULL, 
+	SoLuongBan		INT NOT NULL, 
+	SoLuocXem		INT NOT NULL, 
+	BiXoa			BIT NOT NULL, 
+	MaLoaiSanPham	INT NOT NULL, 
+	MaHangSanXuat	INT NOT NULL,
+	CONSTRAINT fk_MaLoaiSanPham_LoaiSanPham FOREIGN KEY (MaLoaiSanPham) REFERENCES LoaiSanPham(MaLoaiSanPham),
+	CONSTRAINT fk_MaHangSanXuat_HangSanXuat FOREIGN KEY (MaHangSanXuat) REFERENCES HangSanXuat(MaHangSanXuat)
+);
+GO
+CREATE TABLE DonDatHang(
+	MaDonDatHang	VARCHAR(9) PRIMARY KEY NOT NULL, 
+	NgayLapPhieu	DATETIME NOT NULL, 
+	TongThanhTien	INT NOT NULL, 
+	MaTaiKhoan		INT NOT NULL, 
+	MaTinhTrang		INT NOT NULL,
+	CONSTRAINT fk_MaTinhTrang_TinhTrang FOREIGN KEY (MaTinhTrang) REFERENCES TinhTrang(MaTinhTrang)
+);
+GO
+CREATE TABLE ChiTietDonDatHang(
+	MaChiTietDonDatHang		VARCHAR(11) PRIMARY KEY NOT NULL, 
+	SoLuong					INT NOT NULL, 
+	GiaBan					INT NOT NULL, 
+	MaDonDatHang			VARCHAR(9) NOT NULL, 
+	MaSanPham				INT NOT NULL,
+	CONSTRAINT fk_MaDonDatHang_DonDatHang FOREIGN KEY (MaDonDatHang) REFERENCES DonDatHang(MaDonDatHang),
+	CONSTRAINT fk_MaSanPham FOREIGN KEY (MaSanPham) REFERENCES SanPham(MaSanPham)
+);
